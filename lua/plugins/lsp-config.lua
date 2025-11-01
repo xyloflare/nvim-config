0f0a33380ce7c7fcdad63b45897c02bf220a7156
xyloflare <pro.dev.mohit@gmail.com>
2025-11-01 14:23:19 +0530

    updates for lsp for nvim 11 and autotag plugin

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,85 +1,184 @@
 return {
 	{
-		"williamboman/mason.nvim",
-		lazy = false,
+		"hrsh7th/cmp-nvim-lsp",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{ "antosha417/nvim-lsp-file-operations", config = true },
+			{ "folke/lazydev.nvim", opts = {} },
+		},
 		config = function()
-			require("mason").setup()
+			-- import cmp-nvim-lsp plugin
+			local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+			-- used to enable autocompletion (assign to every lsp server config)
+			local capabilities = cmp_nvim_lsp.default_capabilities()
+
+			vim.lsp.config("*", {
+				capabilities = capabilities,
+			})
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>mn", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>rf", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
 		opts = {
-			auto_install = true,
+			-- list of servers for mason to install
+			ensure_installed = {
+				"ts_ls",
+				"html",
+				"cssls",
+				"tailwindcss",
+				--"svelte",
+				"lua_ls",
+				--"graphql",
+				--"emmet_ls",
+				--"prismals",
+				--"pyright",
+				"eslint",
+			},
 		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-			local lspconfig = require("lspconfig")
-			local util = require("lspconfig/util")
-			lspconfig.rust_analyzer.setup({
-				filetypes = { "rust" },
-				root_dir = util.root_pattern("Cargo.toml"),
-				--on_attach = function(client, bufnr)
-				--vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-				--vim.api.nvim_buf_set_option("n", "<leader>ca", vim.lsp.buf.code_action, {})
-				--end,
-				capabilities = capabilities,
-				settings = {
-					["rust-analyzer"] = {
-						cargo = {
-							allFeatures = true,
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				opts = {
+					ui = {
+						icons = {
+							package_installed = "✓",
+							package_pending = "➜",
+							package_uninstalled = "✗",
 						},
 					},
 				},
-			})
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-			})
-      lspconfig.astro.setup({
-        capabilities = capabilities
-      })
-      lspconfig.gopls.setup({})
-
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>mn", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>rf", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
+			},
+			"neovim/nvim-lspconfig",
+		},
 	},
 	{
-		"rust-lang/rust.vim",
-		config = function()
-			vim.g.rustfmt_autosave = 1
-		end,
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				--"isort", -- python formatter
+				--"black", -- python formatter
+				--"pylint",
+				"eslint_d",
+			},
+		},
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
 	},
+}
+--[[
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local lspconfig = vim.lsp.config --require("lspconfig")
+      local util = require("lspconfig/util")
+      lspconfig.rust_analyzer.setup({
+        filetypes = { "rust" },
+        root_dir = util.root_pattern("Cargo.toml"),
+        --on_attach = function(client, bufnr)
+        --vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        --vim.api.nvim_buf_set_option("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        --end,
+        capabilities = capabilities,
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+            },
+          },
+        },
+      })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+      })
+      --lspconfig.astro.setup({
+        --capabilities = capabilities,
+      --})
+      lspconfig.gopls.setup({})
+      --lspconfig.lua
+      lspconfig.clangd.setup({
+        cmd = { "clangd", "--compile-commands-dir=" .. vim.loop.cwd() },
+        filetypes = { "c", "cpp", "objc", "objcpp", "arduino" },
+        init_options = {
+          usePlaceholders = true,
+          completeUnimported = true,
+        },
+      })
+      lspconfig.arduino_language_server.setup({
+        cmd = {
+          "arduino-language-server",
+          "-clangd",
+          "/usr/bin/clangd",
+          "-cli",
+          "/usr/bin/arduino-cli",
+          "-cli-config",
+          "/home/xylo/.arduino15/arduino-cli.yaml",
+          "-fqbn",
+          "arduino:avr:nano",
+        },
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>mn", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>rf", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    config = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
   {
     "wuelnerdotexe/vim-astro",
   },
-	--  {
-	--    "sigmasd/deno-nvim",
-	--    config = function ()
-	--      local util = require("lspconfig/util")
-	--      require('deno-nvim').setup(
-	--        {
-	--          root_dir = util.root_pattern("mod.ts", "mod.js"),
-	--        });
-	--    end
-	--  },
-	--[[
+  --  {
+  --    "sigmasd/deno-nvim",
+  --    config = function ()
+  --      local util = require("lspconfig/util")
+  --      require('deno-nvim').setup(
+  --        {
+  --          root_dir = util.root_pattern("mod.ts", "mod.js"),
+  --        });
+  --    end
+  --  },
+  --[[
   {
     "mrcjkb/rustaceanvim",
     version = "^4",
@@ -115,6 +214,5 @@ return {
       }
     end,
   },
-  ]]
-	--
-}
+}]]
+--
